58b2c95ccd29184e91acfe7c7c4431c0d5502bf2
Brandon Leeds <brandonleeds@gmail.com>
2018-02-21 15:42:58 +0000

    automatically backed up by learn

--- db/migrate/20180221154102_create_recipes.rb
+++ db/migrate/20180221154102_create_recipes.rb
@@ -3,7 +3,7 @@ class CreateRecipes < ActiveRecord::Migration
     create_table :recipes do |t|
       t.string :name
       t.string :ingredients
-      t.int :cook_time
+      t.double :cook_time
     end
   end
 end
