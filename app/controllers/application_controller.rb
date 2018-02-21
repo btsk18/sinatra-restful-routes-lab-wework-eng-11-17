aa755e91f91a6de33c1123bc1177a8af1055ff6a
Brandon Leeds <brandonleeds@gmail.com>
2018-02-21 16:28:55 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -47,6 +47,7 @@ class ApplicationController < Sinatra::Base
     @recipe = Recipe.create(:name => params[:name],
     :ingredients => params[:ingredients],
     :cook_time => params[:cook_time])
+    redirect '/recipes/#{@recipe.id}'
   end
 
 end
